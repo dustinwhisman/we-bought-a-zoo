b3c9ce3e06c5df77d95839fdabc7489ccaf12b88
Dustin Whisman <whisman.dustin@gmail.com>
2022-12-10 16:52:39 -0700

    chore: add .editorconfig

--- src/scripts/counter/index.test.ts
+++ src/scripts/counter/index.test.ts
@@ -17,60 +17,60 @@ describe('counter', () => {
       document.body.innerHTML = '<button type="button" data-count="4">count is 4</button>';
       context.element = document.querySelector('button');
     });
-  
+
     it.concurrent('element is not null', (context) => {
       expect(context.element).not.toBe(null);
     })
-  
+
     it.concurrent('updateElement updates the count', (context) => {
       updateElement(context.element, 5);
       expect(context.element?.dataset.count).toBe('5');
       expect(context.element?.innerHTML).toBe('count is 5');
     });
-  
+
     it.concurrent('getCurrentCount returns the correct count', (context) => {
       const count = getCurrentCount(context.element);
       expect(count).toBe(4);
     });
   });
-  
+
   describe('counter with bad initial state', () => {
     beforeEach((context) => {
       document.body.innerHTML = '<button type="button"></button>';
       context.element = document.querySelector('button');
     });
-  
+
     it.concurrent('element is not null', (context) => {
       expect(context.element).not.toBe(null);
     })
-  
+
     it.concurrent('updateElement updates the count', (context) => {
       updateElement(context.element, 5);
       expect(context.element?.dataset.count).toBe('5');
       expect(context.element?.innerHTML).toBe('count is 5');
     });
-  
+
     it.concurrent('getCurrentCount returns 0', (context) => {
       const count = getCurrentCount(context.element);
       expect(count).toBe(0);
     });
   });
-  
+
   describe('counter with null element given', () => {
     beforeEach((context) => {
       document.body.innerHTML = '<button type="button" data-count="4">count is 4</button>';
       context.element = document.querySelector('.button');
     });
-  
+
     it.concurrent('element to be null', (context) => {
       expect(context.element).toBe(null);
     })
-  
+
     it.concurrent('updateElement does nothing', (context) => {
       updateElement(context.element, 5);
       expect(context.element).toBe(null);
     });
-  
+
     it.concurrent('getCurrentCount returns 0', (context) => {
       const count = getCurrentCount(context.element);
       expect(count).toBe(0);
